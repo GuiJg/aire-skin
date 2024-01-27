9f9fdc1d39408d9e333118c74a9c0d1930c48ef5
GuiJg <j.gu1lh3rm325@gmail.com>
2024-01-27 11:13:57 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+// Seleciona o header pelo ID
+const header = document.getElementById("header-aire");
+
+// Obter a altura do header
+const headerHeight = header.clientHeight;
+
+// Adiciona um evento de rolagem à janela
+window.onscroll = function () {
+    // Verifica a posição de rolagem da página
+    if (window.scrollY > 0) {
+        // Se estiver abaixo do topo, esconde .header-menu e .div-header
+        header.classList.add("scrolled");
+        header.style.transition = "background-color 0.5s ease";
+        header.style.backgroundColor = "#983304ce";
+        header.style.backdropFilter = "blur(5px)"
+        document.querySelector('.input').style.borderBottomColor = '#dac894';
+        document.querySelector('.header-logo img').src = 'assets/AIRE logo.png';
+    } else {
+        // Se estiver no topo, mostra .header-menu e .div-header
+        header.classList.remove("scrolled");
+        header.style.transition = "background-color 0.5s ease";
+        header.style.backgroundColor = "";
+        header.style.backdropFilter = "blur(0px)"
+        document.querySelector('.input').style.borderBottomColor = '#983404';
+        document.querySelector('.header-logo img').src = 'assets/AIRE logo marrom.png'; 
+    }
+};
